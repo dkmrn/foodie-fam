c97256969c0605d71cc9be09368e5c78f0a7752b
katiachoi <katia.choi818@gmail.com>
2025-02-23 15:56:50 -0800

    Create Post button added

--- mern/client/src/App.jsx
+++ mern/client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import DummyPost from "./components/dummyPost";
+ import { GoToCreate } from "./components/dummyCreate";
 import { useState } from 'react';
 
 
@@ -7,13 +8,16 @@ import { useState } from 'react';
 const App = () => {
   // Sample array of items 
   // NEED TO UPDATE THIS ACCORDING TO EACH NEW POST
-  const items = ["Pizza", "Burger", "Pasta", "Sushi", "Tacos"];
+  const items = ["Pizza", "Burger", "Pasta", "Sushi", "Tacos","test", "test"];
 
   return (
     <div className="container">
       <header className="header">
         <h1>Find your group</h1>
         <p>Say hi!</p>
+        <div className="create-container">
+          <GoToCreate />
+        </div>
       </header>
       <div className="grid">
         {items.map((item,index) => (
