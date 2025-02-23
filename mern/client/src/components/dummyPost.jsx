f204d471b47225d7edb8737628cedd7b3964a031
1004yeeun <joycejeoung@gmail.com>
2025-02-23 15:23:21 -0800

    dummyPost update

--- mern/client/src/components/dummyPost.jsx
+++ mern/client/src/components/dummyPost.jsx
@@ -3,11 +3,34 @@ return (
     <div style={{ 
         width: 'min(50vw, 50vh)', 
         height: 'min(50vw, 50vh)', 
-        borderRadius: 10, 
-        backgroundColor: "black",
+        borderRadius: '16%', 
+        backgroundColor: "white",
+        border: '5px solid rgb(14, 7, 66)',
         display: 'flex',
-        justifyContent: 'center'
-
-    }}></div>
+        justifyContent: 'center',
+        alignItems: 'flex-start'
+    }}>
+        {/*inside the box*/}
+        <div style={{
+            padding: '6%',
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center'
+        }}>
+            {/*profile picture icon*/}
+            <img 
+                src="https://picsum.photos/id/237/200/300"
+                alt="profile picture"
+                style={{
+                    width: '5vw',
+                    height: '5vw',
+                    borderRadius: '50%',
+                    objectFit: 'cover',
+                    marginRight: '4%'
+                }}
+            />
+            u
+        </div>
+    </div>
   );
 }
