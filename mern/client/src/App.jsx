33986ddcdfae71032945d065adf5325847a6eada
Daya Kumaran <dayakumaran2012@gmail.com>
2025-02-23 10:52:20 -0800

    testing

---
+++ mern/client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="w-full p-6">
+      <Outlet />
+    </div>
+  );
+};
+export default App;
