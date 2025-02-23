7926d546437fbfb920a74f9abaffdba9479acd3a
amandaan732 <amandaan732@g.ucla.edu>
2025-02-23 14:15:44 -0800

    temporary creation of createPost/gotoCreate in dummies

--- mern/client/src/App.jsx
+++ mern/client/src/App.jsx
@@ -1,12 +1,29 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import {GoToCreate} from "./components/dummyCreate";
+import {CreatePost} from "./components/dummyCreatePost";
+
+// const App = () => {
+//   return (
+//     <div className="w-full p-6">
+//       <Navbar />
+//       <Outlet />
+//     </div>
+//   );
+// };
+// export default App;
+
+
+
 
 const App = () => {
   return (
     <div className="w-full p-6">
-      <Navbar />
+      <GoToCreate />
+      <CreatePost />
       <Outlet />
     </div>
   );
 };
 export default App;
+
