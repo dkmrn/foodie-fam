7f3ae1b1c1009a93e2b7fbc962b30b8b7a9901b3
Joaquin Chun <joaquinmchun@gmail.com>
2025-02-24 18:02:04 -0800

    Post on front end succesfully adds to Atlas collection posts

--- mern/server/server.js
+++ mern/server/server.js
@@ -4,8 +4,12 @@ import records from "./routes/record.js";
 import users from "./routes/user.js";
 import posts from "./routes/posts.js";
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const PORT = process.env.PORT || 5050;
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
@@ -14,18 +18,18 @@ app.use("/user", users);
 app.use("/posts", posts);
 
 
-// Enable CORS for requests from localhost:5173
-app.use(cors({
-  origin: "http://localhost:5173", // Allows only localhost:5173 to access your server
-  methods: ["GET", "POST", "PATCH", "DELETE"], // Allowed methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-}));
+// // Enable CORS for requests from localhost:5173
+// app.use(cors({
+//   origin: "http://localhost:5173", // Allows only localhost:5173 to access your server
+//   methods: ["GET", "POST", "PATCH", "DELETE"], // Allowed methods
+//   allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+// }));
 
-// Your other middleware and routes go here
+// // Your other middleware and routes go here
 
-app.listen(5050, () => {
-  console.log("Server running on http://localhost:5050");
-});
+// app.listen(5050, () => {
+//   console.log("Server running on http://localhost:5050");
+// });
 
 
 // start the Express server
