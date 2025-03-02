b9628af78c3d9326d37e4a80683b8c4b77bd8f42
1004yeeun <joycejeoung@gmail.com>
2025-03-02 14:10:03 -0800

    created new pages

---
+++ mern/client/src/components/MyProfile.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+export function MyProfile() {
+    return (
+        <h1>Look at your profile here!</h1>
+    );
+}
