9db475f595a8ed08d23360f5d5d52b419607fcc3
amandaan732 <amandaan732@g.ucla.edu>
2025-03-02 13:20:42 -0800

    Adding CreatePostPage.jsx

---
+++ mern/client/src/CreatePostPage.jsx
@@ -0,0 +1,12 @@
+import { CreatePost } from "./components/dummyCreatePost";
+import React from "react";
+
+const CreatePostPage = () => {
+    return (
+        <div>
+            <CreatePost />
+        </div>
+    )
+}
+
+export default CreatePostPage;
