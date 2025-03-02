94554b777518fc7b15172199664e3bd2b63d3942
Daya Kumaran <dayakumaran2012@gmail.com>
2025-03-02 12:49:16 -0800

    route for updating profile information

--- mern/server/routes/profile.js
+++ mern/server/routes/profile.js
@@ -1,17 +1,44 @@
 import express from "express";
 import db from "../db/connection.js";
-import { ObjectId } from "mongodb";
+import { ObjectId, ProfilingLevel } from "mongodb";
 import user from "./user.js";
 import posts from "./posts.js";
 
 const router = express.Router();
 
 /*
-myUser: user
+myUserID: userID
 myName: name
 myLocation: location
 myBio: Bio/Info
 myPost: posts []
-myJoinedPost: joinedPosts []
+myJoinedPosts: joinedPosts []
 */
 
+
+// 
+
+
+
+
+// This route is for updating basic user info: Name, bio, location, profile pic, 
+router.patch("/profile/:id/update", async (req, res) => {
+    try {
+      const query = { myUserID: req.params.userID };
+      const updates = {
+        $set: {
+          name: req.body.name,
+          myBio: req.body.bio,
+          myLocation: req.body.location,
+        },
+      };
+  
+      let collection = await db.collection("profiles");
+      let result = await collection.updateOne(query, updates);
+      res.send(result).status(200);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Error updating profile information");
+    }
+  });
+  
