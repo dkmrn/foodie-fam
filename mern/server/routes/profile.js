73c7b3292e3e4198816d4a8e326f81b86e9511a3
Joaquin Chun <joaquinmchun@gmail.com>
2025-03-02 11:51:50 -0800

    profiles router.post

--- mern/server/routes/profile.js
+++ mern/server/routes/profile.js
@@ -14,4 +14,33 @@ myBio: Bio/Info
 myPost: posts []
 myJoinedPost: joinedPosts []
 */
+router.post("/", async (req, res) => {
+    try {
+        const userCollection = await db.collection("users");
+        const user = await userCollection.findOne({ _id: new ObjectId(req.body.myUser._id) });
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+
+        let newDocument = {
+            myUser: user,
+            myName: req.body.name,
+            myLocation: req.body.location,
+            myBio: req.body.bio,
+            myPosts: [],
+            myJoinedPosts: []
+        };
+
+        let collection = await db.collection("profiles");
+        let result = await collection.insertOne(newDocument);
+        let profile = await collection.findOne({ _id: result._id });
+        res.send(profile);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error adding profile");
+    }
+});
+
+
+export default router;
 
