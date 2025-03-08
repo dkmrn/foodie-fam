8dd042400c6006b6c3e223e46df7bed6efd1e4cb
katiachoi <katia.choi818@gmail.com>
2025-03-08 15:48:53 -0800

    git testing

--- mern/client/src/App.jsx
+++ mern/client/src/App.jsx
@@ -4,24 +4,49 @@ import { GoToCreate } from "./components/Create";
 import { Logout } from "./components/logout";
 import  { ProfileButton } from "./components/profileButton";
 import { ReportButton } from "./components/reportButton";
-import { useState } from 'react';
-import {SubmitReport } from "./components/SubmitReport";
+import { useEffect, useState } from 'react';
+import { fetchPosts } from "./api/Posts";
 
+// Post data structure
+/*
+    _id: "someMongoDBId",
+    name: "restaurant name",
+    location: "restaurant location",
+    date: "the date",
+    time: "the time",
+    listerId: "id of user who created post",
+    participants: [] 
+
+*/
+
+// hello test
 
 const App = () => {
   // Sample array of items 
   // NEED TO UPDATE THIS ACCORDING TO EACH NEW POST
+  const users = ["Katia", "Joyce", "Amanda", "Daya", "Joaquin"];
 
+  const [postArray, setPostArray] = useState([]);
 
-  const [isReportOpen, setIsReportOpen] = useState(false);
-  const openReport = () => {
-    setIsReportOpen(true);
-  };
-  const closeReport = () => {
-    setIsReportOpen(false);
-  };
+  useEffect(() => 
+  {
+    async function getPostArray()
+    {
+      try
+      {
+        const postArray = await fetchPosts();
+        setPostArray(postArray);
+      }
+      catch(error)
+      {
+        console.error("Failed to fetch posts:", error);
+      };
+    };
+    getPostArray();
+  },[]);
 
-  const array = ["test"];
+  console.log(postArray);
+
 
   return (
     <div className="container">
@@ -44,26 +69,21 @@ const App = () => {
         </div>
 
         <div className="report-button">
-          <ReportButton onClick={openReport}/>
+          <ReportButton />
         </div>
       </header>
 
-      {isReportOpen && (
-      <div className="popup">
-        <div className="popup-inside">
-          <button className="exit-report" onClick={closeReport}>
-            X
-          </button>
-          <SubmitReport />
-        </div>
-      </div>
-      )}
 
       <div className="grid">
-        {array.map((userID,index) => (
+
+        {users.map((user,index) => (
           <div 
-            key = {index}>
-        <Post username ={userID}/>
+            key = {index}
+        >
+        <
+          Post
+          username ={user}
+        />
           </div>
         ))}
       </div>
