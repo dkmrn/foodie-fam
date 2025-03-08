37d581805464c4e948e8d27cc3ba096f6bcab325
1004yeeun <joycejeoung@gmail.com>
2025-03-08 13:59:51 -0800

    implemented Login.jsx to take and store user input for username and password

--- mern/client/src/components/Login.jsx
+++ mern/client/src/components/Login.jsx
@@ -1,8 +1,54 @@
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
+// make the login page auto refresh
+
 export function Login() {
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+    
+    const handleChange = (info) => {
+        setFormData((formData) => ({
+            ...formData,
+            [info.target.name]: info.target.value
+        }));
+    };
+    
+    const handleSubmit = () => {
+        setIsSubmitted(true);
+        console.log("user email is: ", formData.email);
+        console.log("user pass is: ", formData.password);
+    };
+    
     return (
-        <h1>Login Here!</h1>
+        <div className="login-box">
+            <h1>Login Here!</h1>
+            <div>
+                <p>email address: {formData.email}</p>
+                <input 
+                    name="email" 
+                    value={formData.email}
+                    onChange={handleChange}
+                    type='email'
+                    placeholder="enter your email..."
+                    disabled={isSubmitted}
+                />
+            </div>
+        <div>
+        <p>password: {formData.password}</p>
+        <input
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            type='password'
+            placeholder="enter your password..."
+            disabled={isSubmitted}
+        />
+        </div>
+        <button className="login-buttons" type='submit' onClick={handleSubmit}>Submit</button>
+    </div>
     );
-}
+};
