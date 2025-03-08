fd65bed8aaa86bec80d926e2f46b714d810c012f
Daya Kumaran <dayakumaran2012@gmail.com>
2025-03-08 13:55:12 -0800

    login user

--- mern/client/src/api/Users.js
+++ mern/client/src/api/Users.js
@@ -19,3 +19,26 @@ export const sendUser = async (userData) => {
         throw error;
     }
 };
+
+export const loginUser = async (userData) => {
+    try{
+        const response = await fetch("http://localhost:5050/user/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        });
+
+        if (!response.ok) {
+            throw new Error("Login failed");
+        }
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error("Login Error:", error);
+        throw error;
+    }
+};
+
