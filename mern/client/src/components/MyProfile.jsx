d548959e388587a1735fc5f800ca32381a07e83a
Joaquin Chun <joaquinmchun@gmail.com>
2025-03-08 19:10:39 -0800

    All information about my profile. Then gettign informations about all myPost & joinedPosts

--- mern/client/src/components/MyProfile.jsx
+++ mern/client/src/components/MyProfile.jsx
@@ -1,7 +1,79 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './MyProfile.css';
+import { getUserId } from "../main";
+import { getProfile } from "../api/Profiles";
+import { getPost } from "../api/Posts";
 
 export function MyProfile() {
+
+    const [profile, setProfile] = useState(null);
+    const [myPosts, setMyPosts] = useState([]);
+    const [joinedPosts, setJoinedPosts] = useState([]);
+
+    useEffect(() =>
+    {
+        async function fetchProfile(userId)
+        {
+            console.log("User Id: ",userId);
+            try
+            {
+                const profile = await getProfile(userId);
+                setProfile(profile);
+            }
+            catch(error)
+            {
+                console.error("Failed to fetch profile:", error);
+            }
+        }
+        fetchProfile(getUserId())
+    },[]);
+    console.log(profile);
+
+    useEffect(() =>
+    {
+        if (!profile || !profile.myPosts) return; // Ensure profile is loaded
+        async function fetchMyPosts()
+        {
+            try
+            {
+                for (const myPost of profile.myPosts)
+                    {
+                        const post = await getPost(myPost);
+                        setMyPosts(prevPosts => [...prevPosts, post]);
+                    }
+            }
+            catch(error)
+            {
+                console.error("Failed to fetch my posts:", error);
+            }
+        }
+        fetchMyPosts();
+    },[profile]);
+    console.log(myPosts);
+
+    useEffect(() =>
+        {
+            if (!profile || !profile.myPosts) return; // Ensure profile is loaded
+            async function fetchJoinedPosts()
+            {
+                try
+                {
+                    for (const joinedPost of profile.myJoinedPosts)
+                        {
+                            const post = await getPost(joinedPost);
+                            setJoinedPosts(prevPosts => [...prevPosts, post]);
+                        }
+                }
+                catch(error)
+                {
+                    console.error("Failed to fetch joined posts:", error);
+                }
+            }
+            fetchJoinedPosts();
+        },[profile]);
+    console.log(joinedPosts);
+
+
 return (
     <div className="profile">
         <img
